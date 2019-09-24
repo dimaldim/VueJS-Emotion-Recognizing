bc6c6ef363bb7e63d7be1aae5dad8465f6ceb397
dimaldim <antraks92@gmail.com>
2019-09-24 17:48:59 +0300

    :hammer: Add comments.

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container>
+    <!-- Show error if cannot detect user's web camera !-->
     <v-row v-if="error">
       <v-col>
         <v-alert outlined type="error" prominent border="left">
@@ -8,8 +9,11 @@
         </v-alert>
       </v-col>
     </v-row>
+    <!-- !-->
+    <!-- Show camera, device select list and "Capture photo" button !-->
     <v-row v-else>
       <v-col>
+        <!-- Display camera !-->
         <code v-if="device">{{ device.label }}</code>
         <vue-web-cam
           v-if="!error"
@@ -20,7 +24,7 @@
           @cameras="onCameras"
           @camera-change="onCameraChange"
         />
-
+        <!-- Display "Select device" list !-->
         <v-select
           :items="devices"
           v-model="camera"
@@ -28,7 +32,7 @@
           item-text="label"
           item-value="deviceId"
         ></v-select>
-
+        <!-- Display "Capture photo" button !-->
         <div class="text-center">
           <v-btn
             small
@@ -40,6 +44,8 @@
         </div>
       </v-col>
     </v-row>
+    <!-- !-->
+    <!-- Display error if cannot recognize face or emotions !-->
     <v-row>
       <v-col>
         <v-img :src="img"></v-img>
@@ -53,6 +59,8 @@
         </v-alert>
       </v-col>
     </v-row>
+    <!-- !-->
+    <!-- Display points, and emotions buttons !-->
     <v-row v-if="myExpression.length && !myExpressionError.length">
       <v-col>
         <v-alert
@@ -81,7 +89,10 @@
         </v-alert>
       </v-col>
     </v-row>
-    <v-row class="text-center" v-if="myExpression.length && !myExpressionError.length">
+    <v-row
+      class="text-center"
+      v-if="myExpression.length && !myExpressionError.length"
+    >
       <v-col>
         <v-btn
           fab
@@ -145,6 +156,7 @@
         </v-btn>
       </v-col>
     </v-row>
+    <!-- !-->
   </v-container>
 </template>
 
@@ -179,18 +191,10 @@ export default {
     };
   },
   computed: {
+    /* Load store getters */
     ...mapGetters(["isLoading", "gamePoints"]),
     device: function() {
       return this.devices.find(n => n.deviceId === this.deviceId);
-    },
-    userAnswerClass: function() {
-      return {
-        alert: true,
-        "alert-danger": !this.isAnswerRight,
-        "alert-success": this.isAnswerRight,
-        "alert-dismissible": true,
-        "fade show": true
-      };
     }
   },
   watch: {
@@ -207,25 +211,25 @@ export default {
     }
   },
   methods: {
+    /* Load store actions */
     ...mapActions(["changeIsLoading", "addPoints"]),
     textToSpeech(text) {
+      /*
+      Load speechSynthesis library, it's used to speak given text.
+      */
       if ("speechSynthesis" in window) {
         let synthesis = window.speechSynthesis;
-
         // Get the first `en` language voice in the list
         let voice = synthesis.getVoices().filter(function(voice) {
           return voice.lang === "en-US";
         })[0];
-
         // Create an utterance object
         var utterance = new SpeechSynthesisUtterance(text);
-
         // Set utterance properties
         utterance.voice = voice;
         utterance.pitch = 1.5;
         utterance.rate = 1;
         utterance.volume = 0.8;
-
         // Speak the utterance
         synthesis.speak(utterance);
       } else {
@@ -233,19 +237,28 @@ export default {
       }
     },
     async onCapture() {
-      this.changeIsLoading();
-      this.resetState();
-      this.img = this.$refs.webcam.capture();
-      const myImg = document.getElementById("my_img");
+      /*
+      Triggers when "Capture photo" button is clicked.
+      */
+      this.changeIsLoading(); // show loading circle
+      this.resetState(); // reset user state
+      this.img = this.$refs.webcam.capture(); // capture photo
+      const myImg = document.getElementById("my_img"); // this element is hidden, not visible by end user.
 
-      await faceapi.nets.tinyFaceDetector.loadFromUri("/data/weights");
-      await faceapi.nets.faceExpressionNet.loadFromUri("/data/weights");
+      await faceapi.nets.tinyFaceDetector.loadFromUri("/data/weights"); // load face models
+      await faceapi.nets.faceExpressionNet.loadFromUri("/data/weights"); // load face expression models
       const detections = await faceapi
         .detectAllFaces(myImg, new faceapi.TinyFaceDetectorOptions())
-        .withFaceExpressions();
+        .withFaceExpressions(); // detect face expressions
+      /*
+      If cannot detect face or face expressions throw error.
+      */
       if (!detections.length) {
         this.myExpressionError = "Cannot find face or detect expressions";
       } else {
+        /*
+        If everything is ok - load expressions.
+        */
         this.expressions = {
           happy: detections[0].expressions.happy,
           angry: detections[0].expressions.angry,
@@ -253,39 +266,59 @@ export default {
           surprised: detections[0].expressions.surprised,
           neutral: detections[0].expressions.neutral
         };
-        this.myExpression = this.findMyEmotion(this.expressions);
-        this.myExpressionError = "";
+        this.myExpression = this.findMyEmotion(this.expressions); // set myExpressions to detected emotion.
+        this.myExpressionError = ""; // if any error - set to blank.
       }
-      this.changeIsLoading();
+      this.changeIsLoading(); // hide loading circle.
     },
     onError(error) {
+      /*
+      Load errors, if any.
+      */
       this.error = error;
     },
     onCameras(cameras) {
+      /*
+      Load devices
+      */
       this.devices = cameras;
     },
     onCameraChange(deviceId) {
+      /*
+      Change camera(device) on change in "Select device" list.
+      */
       this.deviceId = deviceId;
       this.camera = deviceId;
     },
     findMyEmotion(obj) {
+      /*
+      Returns detected face emotion.
+      */
       return Object.keys(obj).reduce((a, b) => (obj[a] > obj[b] ? a : b));
     },
     checkEmotion(emotion) {
+      /*
+      Check user answer.
+      This function decides whether user answer is correct based on the detected emotion.
+      */
       this.userAnswer = emotion;
       this.isAnswerRight = emotion == this.myExpression;
       if (emotion == this.myExpression) {
-        this.addPoints(1);
+        this.addPoints(1); // Add 1 point if user answer is correct
         this.textToSpeech(
           `Yes, I’m ${this.userAnswer}. Can you make the same face?`
-        );
+        ); // Text-To-Speech
       } else {
         this.textToSpeech(
           `Sorry, but I'm not ${this.userAnswer}. Please try again.`
-        );
+        ); // Text-To-Speech
       }
     },
     resetState() {
+      /*
+      Reset errors, user answer and face expressions
+      It is used when new photo is captured.
+       */
       this.myExpressionError = "";
       this.userAnswer = "";
       this.isAnswerRight = false;
